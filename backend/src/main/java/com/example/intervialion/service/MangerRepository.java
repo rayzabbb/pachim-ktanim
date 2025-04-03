118b8a4f10a9b40fb9e0a5b119cda6861f2e4bd8
rayzabbb <144956875+rayzabbb@users.noreply.github.com>
2025-04-03 22:21:43 +0300

    fix src dir

---
+++ backend/src/main/java/com/example/intervialion/service/MangerRepository.java
@@ -0,0 +1,6 @@
+package com.example.intervialion.service;
+import com.example.intervialion.model.Manger;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MangerRepository extends JpaRepository<Manger, Long> {
+}
--- backend/src/src/main/java/com/example/intervialion/service/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.intervialion.service;
-import com.example.intervialion.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository <Category, Long>{
-
-}
